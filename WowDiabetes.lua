aa9498dc3fb048ef8bd330178060c1d43d2ccb5c
Zachary Hoefler <zachary.hoefler@gmail.com>
2013-09-22 00:44:07 -0400

    AddOn now detects eating and drinking

--- WowDiabetes.lua
+++ WowDiabetes.lua
@@ -1,3 +1,11 @@
+-- Utility functions/variables
+local L = WowDiabetesLocalization
+
+local function ColorPrint(text, color)
+	color = color or "ffffff00" -- Default to yellow
+	print("|c" .. color .. text)
+end
+
 -- Called when the main frame first loads
 function WowDiabetes_OnLoad(frame)
 	-- Combat enter/leave
@@ -16,24 +24,34 @@ end
 function WowDiabetes_OnEvent(frame, event, ...)
 	if event == "PLAYER_REGEN_DISABLED" then
 		-- Combat entered
-		print("Combat entered")
+		ColorPrint("Combat entered")
 	elseif event == "PLAYER_REGEN_ENABLED" then
 		-- Combat exited
-		print("Combat exited")
+		ColorPrint("Combat exited")
 	elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
 		-- Spell cast; may be food/drink consumed
-		print("Spell cast")
+		WowDiabetes_HandleSpellCast(...)
 	elseif event == "UNIT_AURA" then
 		-- Player buffs changed, may be due to food/drink
-		print("Auras changed")
+		ColorPrint("Auras changed")
 	elseif event == "BAG_UPDATE" then
 		-- Bags have changed; check for food/drink consumed if needed
-		print("Bags changed")
+		ColorPrint("Bags changed")
 	end
 end
 
 -- Called whenever the user clicks on the main WowDiabetes frame
 function WowDiabetes_OnClickFrame()
-	print("Frame clicked")
+	ColorPrint("Frame clicked")
 end
 
+function WowDiabetes_HandleSpellCast(unitId, spell, rank, lineId, spellId)
+	if unitId == "player" then
+		-- Check for food/drink
+		if spell == L["DRINK_AURA_NAME"] then
+			ColorPrint("Player is about to drink")
+		elseif spell == L["FOOD_AURA_NAME"] then
+			ColorPrint("Player is about to eat")
+		end
+	end
+end
