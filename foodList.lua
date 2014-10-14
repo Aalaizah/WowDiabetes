c17bacbb32b7d5607193d0244a28b66463bf70c9
Alex <amm4108@gmail.com>
2014-10-14 17:00:35 -0400

    added some more food ids, update for 6.0
    Updated the TOC so it works with 6.0, added ids for some more foods so
    when we have more food values it's easy to add in

--- foodList.lua
+++ foodList.lua
@@ -3,23 +3,102 @@ foodList = {
 [4540] = 29.44, --Tough Hunk of Bread
 [4541] = 288, -- Freshly Baked Bread
 [4542] = 244.1, -- Moist Cornbread
+
 --Fish
 [787] = 0, -- Slitherskin Mackerel
 [4592] = 0, -- Longjaw Mud Snapper
 [4593] = 0, -- Bristle Whisker Catfish
---
+
+-- Fungus
 [4604] = 0, -- Forest Mushroom Cap
 [4605] = 0, -- Red-speckled Mushroom
 [4606] = 1, -- Spongy Morel
+
+-- Meat
 [117] = 1, -- Tough Jerky
 [2287] = 0, -- Haunch of Meat
 [3770] = 0, -- Mutton Chop
+
+-- Cheese
 [2070] = 0, -- Darnassian Bleu
 [414] = 0, -- Dalaran Sharp
 [422] = 0, -- Dwarven Mild
+
+-- Fruit
 [4536] = 6.1, -- Shiny Red Apple
 [4537] = 10.4, -- Tel-Abim Banana
 [4538] = 3.6, -- Snapvine Watermelon
+
+--[[
+-- Level 90 Foods
+[104342] -- Spicy Mushan Noodles
+[101746] -- Seasoned Pomfruit Slices
+[101747] -- Farmer's Delight
+[101748] -- Spiced Blossom Soup
+[101749] -- Stuffed Lushrooms
+[101750] -- Fluffy Silkfeather Omelet
+[104339] -- Harmonious River Noodles
+[104340] -- Crazy Snake Noodles
+[104341] -- Steaming Goat Noodles
+[108920] -- Lemon Flower Pudding
+[98116] -- Freeze-Dried Hyena Jerky
+[80618] -- Conjured Mana Buns
+[81408] -- Red Bean Bun
+[81409] -- Tangy Yogurt
+[81410] -- Green Curry Fish
+[81411] -- Peach Pie
+[81412] -- Blanched Needle Mushrooms
+[81413] -- Skewered Peanut Chicken
+[81414] -- Pearl Milk Tea
+[80610] -- Conjured Mana Pudding
+[79320] -- Half a Lovely Apple
+[74646] -- Black Pepper Ribs and Shrimp
+[74648] -- Sea Mist Rice Noodles
+[74650] -- Mogu Fish Stew
+[74653] -- Steamed Crab Surprise
+[74656] -- Chun Tian Spring Rolls
+[74919] -- Pandaren Banquet
+[75016] -- Great Pandaren Banquet
+[75038] -- Mad Brewer's Breakfast
+[81415] -- Pandaren Plum Wine
+[81916] -- Humongous Fungus
+[86508] -- Fresh Bread
+[87226] -- Banquet of the Grill
+[87228] -- Great Banquet of the Grill
+[87230] -- Banquet of the Wok
+[87232] -- Great Banquet of the Wok
+[87234] -- Banquet of the Pot
+[87236] -- Great Banquet of the Pot
+[87238] -- Banquet of the Steamer
+[86074] -- Spicy Vegetable Chips
+[86073] -- Spicy Salmon
+[81920] -- Plump Fig
+[81921] -- Chilton /stilton
+[81923] -- Cobo Cola
+[82344] -- Hearthglen AMrosia
+[82449] -- Barnacle Bouillabaisse
+[82451] -- Frybread
+[83094] -- Foote Tripel
+[81918] -- Pickled Pig's Snout
+[87240] -- Great Banquet of the Steamer
+[98123] -- Whale Shark Caviar
+[98124] -- Bloodberry Tart
+[98125] -- Shaved Zangar Truffles
+[98126] -- Mecahnopeep Foie Gras
+[98127] -- Dented Can of Kaja'Cola
+[98118] -- Scorpion Crunchies
+[98111] -- K.R.E
+[87242] -- Banquet of the Oven
+[87244] -- Great Banquet of the Oven
+[87246] -- Banquet of the Brew
+[87248] -- Great Banquet of the Brew
+[87253] -- Perpetual Leftovers
+[88531] -- lao Chin's Last Mug
+[90135] -- Thick-Cut Bacon
+[94535] -- Grilled Dinosaur Haunch
+[101745] -- Mango Ice
+[104344] -- Lucky Mushroom Noodles
+]]
 }
 
 -- foods we don't have values for yet
@@ -54,27 +133,3 @@ foodList = {
 [111442] -- Sturgeon Stew
 [111443] -- Salted Skulker
 [111443] -- Fat Sleeper Cakes]]
-
---[[
- -- Golden Dragon Noodles
- -- Spicy Mushan Noodles
- -- Seasoned Pomfruit Slices
- -- Farmer's Delight
- -- Spiced Blossom Soup
- -- Stuffed Lushrooms
- -- Fluffy Silkfeather Omelet
- -- Harmonious River Noodles
- -- Crazy Snake Noodles
- -- Steaming Goat Noodles
- -- Lemon Flower Pudding
- -- Freeze-Dried Hyena Jerky
- -- Conjured Mana Buns
- -- Red Bean Bun
- -- Tangy Yogurt
- -- Green Curry Fish
- -- Peach Pie
- -- Blanched Needle Mushrooms
- -- Skewered Peanut Chicken
- -- Pearl Milk Tea
- -- Conjured Mana Pudding
-]]
